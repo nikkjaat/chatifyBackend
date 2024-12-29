a9074a9e0aaa99497a59ae78eb45352cebc25643
Nikhil <nikhiljaat330@gmail.com>
2024-12-29 21:34:28 +0530

    update

--- config/cors.js
+++ config/cors.js
@@ -1,18 +1,17 @@
 const allowedOrigins = [
-  "https://chatifybackend-sytq.onrender.com", // Production URL
-  "http://localhost:3000", // Development URL
+  "https://mychatifyapp.netlify.app", // Frontend URL
+  "http://localhost:3000", // Development URL (optional)
 ];
 
 const cors = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      // Allow requests with no origin (e.g., mobile apps or curl requests)
+    if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  credentials: true, // If you are using cookies or auth headers
   optionsSuccessStatus: 200, // Some browsers (e.g., IE11) choke on 204
 };
 
